4a5aa37a5378ff443826342d9a5e3936cf9db272
Anatoly Batov <play.hoffa@gmail.com>
2024-07-30 15:01:45 +0400

    commit

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+declare(strict_types=1);
+
+echo "Hi! I\'m a veb server";
