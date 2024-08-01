091f3bcaf977dfd826e07d4c7a88c8e4ded4396a
Anatoly Batov <play.hoffa@gmail.com>
2024-08-01 19:56:05 +0400

    commit

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 declare(strict_types=1);
-header('Location: http://www.GOOGLE.com/');
+// header('Location: http://www.GOOGLE.com/');
+$name = 'TestCookie';
+$value = 'value_of_cookie';
+setcookie($name, $value);
 
 echo "Hi! I\'m a veb server <br/>" ;
 echo 'Hi';
